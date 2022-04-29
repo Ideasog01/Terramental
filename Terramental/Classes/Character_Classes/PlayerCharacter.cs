481d7781c1585979dd773577c65bc6a87eb9667a
Ideasog01 <oliver.dev01@outlook.com>
2022-04-29 19:52:59 +0100

    Snow Ultimate Update

--- Terramental/Classes/Character_Classes/PlayerCharacter.cs
+++ Terramental/Classes/Character_Classes/PlayerCharacter.cs
@@ -591,11 +591,11 @@ namespace Terramental
 
                         if (!Animations[AnimationIndex].MirrorTexture)
                         {
-                            SpawnManager.SpawnProjectile(_gameManager.GetTexture("Sprites/Projectiles/Fire_Projectile"), SpritePosition + new Vector2(40, 0), new Vector2(32, 32), new Vector2(10, 0), false, true, 3, 6);
+                            SpawnManager.SpawnProjectile(_gameManager.GetTexture("Sprites/Projectiles/Snow_Projectile"), SpritePosition + new Vector2(40, 0), new Vector2(32, 32), new Vector2(10, 0), false, true, 3, 6);
                         }
                         else
                         {
-                            SpawnManager.SpawnProjectile(_gameManager.GetTexture("Sprites/Projectiles/Fire_Projectile"), SpritePosition - new Vector2(40, 0), new Vector2(32, 32), new Vector2(-10, 0), false, true, 3, 6);
+                            SpawnManager.SpawnProjectile(_gameManager.GetTexture("Sprites/Projectiles/Snow_Projectile"), SpritePosition - new Vector2(40, 0), new Vector2(32, 32), new Vector2(-10, 0), false, true, 3, 6);
                         }
 
                         break;
--- Terramental/Classes/Interaction_Classes/Projectile.cs
+++ Terramental/Classes/Interaction_Classes/Projectile.cs
@@ -73,8 +73,7 @@ namespace Terramental
         {
             if(_projectileTrigger == 1)
             {
-                character.SetStatus(BaseCharacter.CharacterStatus.Frozen, 2, 0, 0);
-                //character.SetStatus(BaseCharacter.CharacterStatus.Burning, 5, 2, 20);
+                character.SetStatus(BaseCharacter.CharacterStatus.Burning, 5, 2, 20);
             }
 
             if(_projectileTrigger == 3)
