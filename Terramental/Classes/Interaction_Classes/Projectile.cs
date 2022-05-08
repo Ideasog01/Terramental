bc82dbe7621a80931aff4ae800e0c9adfd508ca5
lllando <lewis.vaillantjones@gmail.com>
2022-05-08 22:29:06 +0100

    COMMEEEENTS

--- Terramental/Classes/Interaction_Classes/Projectile.cs
+++ Terramental/Classes/Interaction_Classes/Projectile.cs
@@ -20,26 +20,26 @@ namespace Terramental
 
         public void UpdateProjectile(GameTime gameTime)
         {
-            SpritePosition += SpriteVelocity;
+            SpritePosition += SpriteVelocity; // Changes the projectile position based on the projectile velocity
 
-            if (_isEnemyProjectile)
+            if (_isEnemyProjectile) // Checks to see if there is an enemy projectile
             {
                 if(_activeDelay <= 0)
                 {
-                    if (OnCollision(_playerCharacter.SpriteRectangle))
+                    if (OnCollision(_playerCharacter.SpriteRectangle))// Checks to see wether the enemy projectile has collided with the player
                     {
                         _playerCharacter.PlayerTakeDamage(_projectileDamage);
                         ProjectileTrigger(_playerCharacter);
                         DestroyProjectile();
                     }
 
-                    foreach (Tile tile in MapManager.activeTiles)
+                    foreach (Tile tile in MapManager.activeTiles) // Loops through every tile in the list of active tiles
                     {
-                        if (tile.IsVisible)
+                        if (tile.IsVisible) // Checks if the tile is visible
                         {
-                            if (tile.IsActive)
+                            if (tile.IsActive) // Checks if the tile is active
                             {
-                                if (OnCollision(tile.SpriteRectangle))
+                                if (OnCollision(tile.SpriteRectangle)) // Checks if the projectile has collided with the tile
                                 {
                                     DestroyProjectile();
                                     break;
@@ -54,49 +54,49 @@ namespace Terramental
 
                 if(_activeDelay <= 0)
                 {
-                    foreach (EnemyCharacter enemy in SpawnManager.enemyList)
+                    foreach (EnemyCharacter enemy in SpawnManager.enemyList) // Loops through every enemy in the list of enemies
                     {
-                        if (enemy.IsActive)
+                        if (enemy.IsActive) // Checks if the enemy is active
                         {
                             if (_activeDelay <= 0)
                             {
-                                if (OnCollision(enemy.SpriteRectangle))
+                                if (OnCollision(enemy.SpriteRectangle)) // Checks if the projectile has collided with an enemy
                                 {
                                     bool damageEnemy = true;
 
-                                    if (_playerCharacter.ElementIndex == 0)
+                                    if (_playerCharacter.ElementIndex == 0) // Checks the player element to see if they are using the fire element
                                     {
-                                        if (enemy.ElementIndex != 2)
+                                        if (enemy.ElementIndex != 2) // Check to see if the enemy is not of snow element type
                                         {
                                             SpawnManager._gameManager.tutorialManager.DisplayIncorrectElementNotification();
-                                            damageEnemy = false;
+                                            damageEnemy = false; // Should not apply damage as the element is invalid
                                         }
                                     }
 
-                                    if (_playerCharacter.ElementIndex == 1)
+                                    if (_playerCharacter.ElementIndex == 1) // Checks the player element to see if they are using the water element
                                     {
-                                        if (enemy.ElementIndex != 0)
+                                        if (enemy.ElementIndex != 0) // Check to see if the enemy is not of fire element type
                                         {
                                             SpawnManager._gameManager.tutorialManager.DisplayIncorrectElementNotification();
-                                            damageEnemy = false;
+                                            damageEnemy = false; // Should not apply damage as the element is invalid
                                         }
                                     }
 
-                                    if (_playerCharacter.ElementIndex == 2)
+                                    if (_playerCharacter.ElementIndex == 2) // Checks the player element to see if they are using the snow element
                                     {
-                                        if (enemy.ElementIndex != 1)
+                                        if (enemy.ElementIndex != 1) // Check to see if the enemy is not of water element type
                                         {
                                             SpawnManager._gameManager.tutorialManager.DisplayIncorrectElementNotification();
-                                            damageEnemy = false;
+                                            damageEnemy = false; // Should not apply damage as the element is invalid
                                         }
                                     }
 
 
-                                    if(damageEnemy)
+                                    if(damageEnemy) // Check to see whether damage should be applied
                                     {
                                         enemy.TakeDamage(_projectileDamage);
 
-                                        if (enemy.CharacterHealth > 0)
+                                        if (enemy.CharacterHealth > 0) // Check to see if the enemy has health remaining
                                         {
                                             ProjectileTrigger(enemy);
                                         }
@@ -109,13 +109,13 @@ namespace Terramental
                         }
                     }
 
-                    foreach (ElementWall elementWall in SpawnManager.elementWallList)
+                    foreach (ElementWall elementWall in SpawnManager.elementWallList) // Loops through every elemental wall in the list of elemental walls
                     {
-                        if (elementWall.IsActive)
+                        if (elementWall.IsActive) // Checks if the elemental wall is active
                         {
                             if (_activeDelay <= 0)
                             {
-                                if (OnCollision(elementWall.SpriteRectangle))
+                                if (OnCollision(elementWall.SpriteRectangle)) // Check to see if the projectile has collided with an elemental wall
                                 {
                                     elementWall.DamageElementWall();
                                     DestroyProjectile();
@@ -125,13 +125,13 @@ namespace Terramental
                         }
                     }
 
-                    foreach (Tile tile in MapManager.activeTiles)
+                    foreach (Tile tile in MapManager.activeTiles) // Loops through every tile in the list of active tiles
                     {
-                        if (tile.IsVisible)
+                        if (tile.IsVisible) // Checks if the tile is visible
                         {
-                            if (tile.IsActive)
+                            if (tile.IsActive) // Checks if the tile is active
                             {
-                                if (OnCollision(tile.SpriteRectangle))
+                                if (OnCollision(tile.SpriteRectangle)) // Checks if the projectile has collided with the tile
                                 {
                                     DestroyProjectile();
                                     break;
@@ -144,7 +144,7 @@ namespace Terramental
 
             if (_destroyTimer > 0)
             {
-                _destroyTimer -= 1 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                _destroyTimer -= 1 * (float)gameTime.ElapsedGameTime.TotalSeconds; // Decreases destroy timer
             }
             else
             {
@@ -153,11 +153,11 @@ namespace Terramental
 
             if(_activeDelay > 0)
             {
-                _activeDelay -= 1 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                _activeDelay -= 1 * (float)gameTime.ElapsedGameTime.TotalSeconds; // Decreases delay timer
             }
         }
 
-        public void ResetProjectile(Texture2D texture, Vector2 position, Vector2 scale, Vector2 velocity, bool isEnemyProjectile, int projectileTrigger, float projectileDuration, int projectileDamage)
+        public void ResetProjectile(Texture2D texture, Vector2 position, Vector2 scale, Vector2 velocity, bool isEnemyProjectile, int projectileTrigger, float projectileDuration, int projectileDamage) // Used to reset a projectile to given parameters
         {
             SpriteTexture = texture;
             SpritePosition = position;
@@ -178,14 +178,14 @@ namespace Terramental
             IsActive = true;
         }
 
-        public void DestroyProjectile()
+        public void DestroyProjectile() // Deletes the projectile
         {
             IsActive = false;
         }
 
         public void ProjectileTrigger(BaseCharacter character)
         {
-            if(_projectileTrigger == 1)
+            if(_projectileTrigger == 1) // Fire projectile
             {
                 character.SetStatus(BaseCharacter.CharacterStatus.Burning, 5, 2, 20);
 
@@ -196,12 +196,12 @@ namespace Terramental
 
             }
 
-            if (_projectileTrigger == 2)
+            if (_projectileTrigger == 2) // Water projectile
             {
                 character.SetStatus(BaseCharacter.CharacterStatus.Empowered, 10, 0, 0);
             }
 
-            if(_projectileTrigger == 3)
+            if(_projectileTrigger == 3) // Snow projectile
             {
                 character.SetStatus(BaseCharacter.CharacterStatus.Frozen, 2, 0, 0);
 
