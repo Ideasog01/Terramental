a311679cbc4047db668132ed67bf3bd97c215c9c
Georgiana Popescu <georgiana.popescu005@gmail.com>
2022-05-08 21:23:53 +0000

    Update

--- Terramental/Classes/Manager_Classes/MapManager.cs
+++ Terramental/Classes/Manager_Classes/MapManager.cs
@@ -25,6 +25,7 @@ namespace Terramental
 
         private List<Texture2D> _waterTileMap = new List<Texture2D>();
         private List<Texture2D> _snowTileMap = new List<Texture2D>();
+        private List<Texture2D> _fireTileMap = new List<Texture2D>();
         private List<Texture2D> _assetWaterList = new List<Texture2D>();
         private List<Texture2D> _assetSnowList = new List<Texture2D>();
         private List<Texture2D> _currentTileMap = new List<Texture2D>();
@@ -157,6 +158,36 @@ namespace Terramental
             _snowTileMap.Add(_gameManager.GetTexture("Tiles/SnowLevel/Ice_Filler")); //23
             _snowTileMap.Add(_gameManager.GetTexture("Tiles/SnowLevel/Ice_Backwards")); //24
 
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/DefaultTile")); //0
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/FireLevel/Fire_Tile")); //0
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/FireLevel/Fire_RightCorner")); //1
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/FireLevel/Fire_RightSlide")); //2
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/FireLevel/Fire_LeftCorner")); //3
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/FireLevel/Fire_LeftSlide")); //4
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/FireLevel/Fire_UpwardsRightCorner")); //5
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/FireLevel/Fire_UpwradsLeftCorner")); //6
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/FireLevel/Fire_UpwardsTile")); //7
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/FireLevel/Fire_Filler")); //8
+            _snowTileMap.Add(_gameManager.GetTexture("Tiles/SnowLevel/Ice_GroundTile")); //9
+            _snowTileMap.Add(_gameManager.GetTexture("Tiles/SnowLevel/Ice_RightCorner")); //10
+            _snowTileMap.Add(_gameManager.GetTexture("Tiles/SnowLevel/Ice_RightSlide")); //11
+            _snowTileMap.Add(_gameManager.GetTexture("Tiles/SnowLevel/Ice_LeftCorner")); //12
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/FireLevel/FireSky_FirstFrame")); //13
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/FireLevel/FireSky_SecondFrame")); //14
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/FireLevel/FireSky_ThirdFrame")); //15
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/FireLevel/FireSky_FourthFrame")); //16
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/FireLevel/FireSky_FifthFrame")); //17
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/FireLevel/FireSky_SeventhFrame")); //18
+            _fireTileMap.Add(_gameManager.GetTexture("Tiles/FireLevel/FireSky_EightFrame")); //19
+            _snowTileMap.Add(_gameManager.GetTexture("Tiles/SnowLevel/Ice_LeftSlide")); //20
+            _snowTileMap.Add(_gameManager.GetTexture("Tiles/SnowLevel/Ice_DownRightCorner")); //21
+            _snowTileMap.Add(_gameManager.GetTexture("Tiles/SnowLevel/Ice_DownLeftCorner")); //22
+            _snowTileMap.Add(_gameManager.GetTexture("Tiles/SnowLevel/Ice_Filler")); //23
+            _snowTileMap.Add(_gameManager.GetTexture("Tiles/SnowLevel/Ice_Backwards")); //24
+
+
+
+
             _assetWaterList.Add(_gameManager.GetTexture("Tiles/DefaultTile")); //0
             _assetWaterList.Add(_gameManager.GetTexture("Assets/WaterLevel/Big_Palm"));
             _assetWaterList.Add(_gameManager.GetTexture("Assets/WaterLevel/Grass_1"));
@@ -180,11 +211,15 @@ namespace Terramental
                 _currentAssetTexture = _assetWaterList;
 
             }
-
             else if (GameManager.levelIndex == 4)
             {
                 _currentTileMap = _snowTileMap;
                 _currentAssetTexture = _assetSnowList;
+            }
+            else if (GameManager.levelIndex == 5)
+            {
+                _currentTileMap = _fireTileMap;
+
             }
 
 
