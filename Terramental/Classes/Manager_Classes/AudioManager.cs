6fe67b35c515d3556b3379b6e562f2b278f79003
Ideasog01 <oliver.dev01@outlook.com>
2022-05-11 17:29:27 +0100

    Audio Manager Comments

--- Terramental/Classes/Manager_Classes/AudioManager.cs
+++ Terramental/Classes/Manager_Classes/AudioManager.cs
@@ -7,46 +7,46 @@ namespace Terramental
 {
     public static class AudioManager
     {
-        public static List<AudioClip> audioLibrary = new List<AudioClip>();
+        public static List<AudioClip> audioLibrary = new List<AudioClip>(); //The list of audio containing all instantiated AudioClips
 
-        public static float musicVolume = 0.1f;
-        public static float sfxVolume = 0.5f;
+        public static float musicVolume = 0.2f; //The default music volume
+        public static float sfxVolume = 0.5f; //The default sound effects volume
 
-        public static bool muteMusic;
-        public static bool muteSfx;
-
-        public static void SetVolumes()
+        public static void SetVolumes() //Sets the initial volumes for sound effects and music
         {
             SoundEffect.MasterVolume = sfxVolume;
             MediaPlayer.Volume = musicVolume;
         }
 
-        public static void AdjustMusicVolume(bool increase)
+        public static void AdjustMusicVolume(bool increase) //Adjusts the volume of music based on the increase boolean. True == Increase, False == Decrease. 
         {
             if (increase)
             {
-                if (musicVolume < 0.9f)
+                if (musicVolume < 0.9f) //Avoids the musicVolume variable to be larger than the maximum volume of 1
                 {
                     musicVolume += 0.1f;
                 }
             }
             else
             {
-                if (musicVolume > 0.1f)
+                if (musicVolume != 0f) //Avoids the musicVolume variable to be smaller than the minimum volume of 0
                 {
                     musicVolume -= 0.1f;
                 }
             }
 
-            MediaPlayer.Volume = musicVolume;
+            if(musicVolume >= 0 && musicVolume <= 1)
+            {
+                MediaPlayer.Volume = musicVolume;
+            }
 
         }
 
-        public static void AdjustSFXVolume(bool increase)
+        public static void AdjustSFXVolume(bool increase) //Adjusts the volume of sound efffects based on the increase boolean. True == Increase, False == Decrease.
         {
             if(increase)
             {
-                if(sfxVolume < 0.9f)
+                if(sfxVolume < 0.9f) //Avoids the sfxVolume variable to be larger than the maximum volume of 1
                 {
                     sfxVolume += 0.1f;
                     PlaySound("BeepTone_SFX");
@@ -54,36 +54,38 @@ namespace Terramental
             }
             else
             {
-                if(sfxVolume > 0.1f)
+                if(sfxVolume != 0f) //Avoids the sfxVolume variable to be smaller than the minimum volume of 0
                 {
                     sfxVolume -= 0.1f;
                     PlaySound("BeepTone_SFX");
                 }
             }
 
-            SoundEffect.MasterVolume = sfxVolume;
-            
+            if(sfxVolume >= 0 && sfxVolume <= 1)
+            {
+                SoundEffect.MasterVolume = sfxVolume;
+            }
         }
 
-        public static void AddSound(AudioClip audioClip)
+        public static void AddSound(AudioClip audioClip) //Adds the AudioClip to the audioLibrary list
         {
             audioLibrary.Add(audioClip);
         }
 
-        public static void PlaySound(string soundName)
+        public static void PlaySound(string soundName) //Plays a sound based on the string variable contents. 
         {
             foreach(AudioClip audioClip in audioLibrary)
             {
-                if(soundName == audioClip.SoundName)
+                if(soundName == audioClip.SoundName) //If the string does not match any audioClip.SoundName, no sound will play.
                 {
-                    if(audioClip.IsMusic && audioClip.SongAudio != null)
+                    if(audioClip.IsMusic && audioClip.SongAudio != null) //If the audio clip is music, use the MediaPlayer class to play the identified audio
                     {
                         MediaPlayer.Play(audioClip.SongAudio);
                         MediaPlayer.IsRepeating = true;
                     }
                     else
                     {
-                        audioClip.EffectAudio.Play();
+                        audioClip.EffectAudio.Play(); //Play the identified sound effect
                     }
 
                     break;
@@ -92,7 +94,7 @@ namespace Terramental
             }
         }
 
-        public static void StopMusic()
+        public static void StopMusic() //Stops the music completely. The music will start at the beginning if the MediaPlayer.Play() is called. If this is not the desired effect, use MediaPlayer.Resume()
         {
             MediaPlayer.Stop();
         }
